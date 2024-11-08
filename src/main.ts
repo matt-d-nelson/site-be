d64658b28c2b5ad2ff7a42ec682d1e6a3d9121b8
Matt Nelson <matt.dav.nelson@gmail.com>
2024-11-08 14:37:38 -0600

    enable local cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true
+  })
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap();
