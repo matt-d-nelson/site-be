4b8fbf2beba8603c4c84aa634eab32077fff4e55
Matt Nelson <matt.dav.nelson@gmail.com>
2025-01-17 13:31:19 -0600

    fix tests

--- src/code-projects/controllers/code-projects.controller.spec.ts
+++ src/code-projects/controllers/code-projects.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { CodeProjectsController } from './code-projects.controller'
+import { CodeProjectsService } from '../services/code-projects.service'
+import { MockCodeProjectsService } from '../models/codeProject.mocks'
+import { CloudinaryService } from 'src/cloudinary/services/cloudinary.service'
+import { MockCloudinaryService } from 'src/cloudinary/services/cloudinary.mocks'
 
 describe('CodeProjectsController', () => {
   let controller: CodeProjectsController
@@ -7,6 +11,16 @@ describe('CodeProjectsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CodeProjectsController],
+      providers: [
+        {
+          provide: CodeProjectsService,
+          useValue: MockCodeProjectsService,
+        },
+        {
+          provide: CloudinaryService,
+          useValue: MockCloudinaryService,
+        },
+      ],
     }).compile()
 
     controller = module.get<CodeProjectsController>(CodeProjectsController)
--- src/code-projects/services/code-projects.service.spec.ts
+++ src/code-projects/services/code-projects.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { CodeProjectsService } from './code-projects.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { CodeProjectEntity } from '../models/codeProject.entity'
+import { MockRepository } from 'src/utils/varios.mocks'
+import { CloudinaryService } from 'src/cloudinary/services/cloudinary.service'
+import { MockCloudinaryService } from 'src/cloudinary/services/cloudinary.mocks'
 
 describe('CodeProjectsService', () => {
   let service: CodeProjectsService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CodeProjectsService],
+      providers: [
+        CodeProjectsService,
+        {
+          provide: getRepositoryToken(CodeProjectEntity),
+          useValue: MockRepository,
+        },
+        {
+          provide: CloudinaryService,
+          useValue: MockCloudinaryService,
+        },
+      ],
     }).compile()
 
     service = module.get<CodeProjectsService>(CodeProjectsService)
